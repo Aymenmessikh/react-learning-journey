0596b8a3b7e2aa7c964531cb299d3a01e8675ab1
MESSIKH Aymen <aymenmessikh.imsi.enpo@gmail.com>
2025-11-07 09:52:33 +0100

    feat: add simple counter

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
+import Counter from "./composants/Counter/Counter.jsx";
 
 function App() {
     return (
-        <h1>hello</h1>
+        <Counter/>
     )
 }
 
---
+++ react-hooks/src/composants/Counter/Counter.jsx
@@ -0,0 +1,27 @@
+import './Counter.css'
+import {useState} from "react";
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    function handlePlusClick() {
+        setCounter(counter + 1);
+    }
+    function handleMinusClick() {
+        setCounter(counter - 1);
+    }
+    function handleResetClick() {
+        setCounter(0);
+    }
+    return (
+        <div className={'Counter'}>
+            <h1 className={'value'}>{counter}</h1>
+            <div className={"button"}>
+                <button onClick={handlePlusClick}>+1</button>
+                <button onClick={handleMinusClick}>-1</button>
+                <button onClick={handleResetClick}>reset</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Counter;
